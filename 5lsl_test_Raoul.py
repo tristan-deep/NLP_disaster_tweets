5bc3ca2a052d076df9ef791df3d58eb518a51b72
raoulmelaet <raoulmelaet@gmail.com>
2020-06-12 16:41:08 +0200

    Fixed the ugly logdir path

--- 5lsl_test_Raoul.py
+++ 5lsl_test_Raoul.py
@@ -3,16 +3,18 @@ from models.LSTM_model import create_model
 from datetime import datetime
 from tensorflow.keras.callbacks import TensorBoard
 import os
+from pathlib import Path
+
 import numpy as np
 
 
 #Make a folder to save model weights, and
-logdir = "logs"
 run = datetime.now().strftime("%Y%m%d-%H%M%S")
+logdir = Path("logs/"+run)
 
 #check if the directory to save the model in exists
-os.makedirs(os.path.dirname(logdir+'/'), exist_ok=True)
-tensorboard_callback = TensorBoard(log_dir=logdir+"\\"+run)
+os.makedirs(os.path.dirname(logdir), exist_ok=True)
+tensorboard_callback = TensorBoard(log_dir=logdir)
 
 
 """Create model"""
@@ -26,4 +28,4 @@ model = create_model(max_words, embedding_vecor_length, max_length, lstm_out)
 
 
 """## Train model"""
-model.fit(gen, epochs=10, callbacks = [tensorboard_callback])
+model.fit(gen, epochs=1, callbacks = [tensorboard_callback])
