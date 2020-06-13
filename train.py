719282310e092b730e3cd9b0271afc7d08ea7631
Tristan <tristan@dewadden.org>
2020-06-13 16:08:12 +0200

    ability to save model

--- train.py
+++ train.py
@@ -10,7 +10,8 @@ import numpy as np
 
 
 if __name__ == '__main__' :
-
+    model_name = 'lstm_embedding-100_out-100-epochs-15'
+    b
     #Make a folder to save model weights, and
     run = datetime.now().strftime("%Y%m%d-%H%M%S")
     logdir = Path("logs/"+run)
@@ -24,7 +25,7 @@ if __name__ == '__main__' :
     vocabulary_size = 1000
     max_length = 100
 
-    embedding_vecor_length = 32
+    embedding_vecor_length = 100
     lstm_out = 100
     tokenizer = TokenizeTweets(max_words=vocabulary_size)
     
@@ -40,3 +41,7 @@ if __name__ == '__main__' :
     
     """## Train model"""
     model.fit(train_gen, validation_data = val_gen, epochs=15, callbacks = [tensorboard_callback])
+    
+    if save_model == True :
+        PATH = Path('weights', model_name)
+        model.save(PATH)
